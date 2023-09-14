cdcc37a4d035cdc920cd7eeabb3458b113d29cfc
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-14 04:48:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.Switch_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number between 1 and 3");
+            int number = int.Parse(Console.ReadLine()); //convert to an int
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("You entered 1");
+                    break;
+                case 2:
+                    Console.WriteLine("You entered 2");
+                    break;
+                case 3:
+                    Console.WriteLine("You entered 3");
+                    break;
+                default: Console.WriteLine("You broke rules");
+                    break;
+
+            }
+            Console.Read();
+
+
+           
+
+
+
+
+        }
+   
+
+    }
+}
